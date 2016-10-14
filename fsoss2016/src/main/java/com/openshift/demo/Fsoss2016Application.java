6238f28a0e24161ed7905a5f2676e26b02d6ca36
Zak Hassan <zak.hassan1010@gmail.com>
2016-10-13 20:10:10 -0400

    Initial Commit of springboot app demo

---
+++ fsoss2016/src/main/java/com/openshift/demo/Fsoss2016Application.java
@@ -0,0 +1,12 @@
+package com.openshift.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fsoss2016Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fsoss2016Application.class, args);
+	}
+}
